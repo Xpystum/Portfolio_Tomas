5d9a37e05dcd1f49b17e76cb09b76da844be61d6
Xpyst <qjq3@mail.ru>
2023-06-14 01:17:26 +0300

    перенёс скрипт наверх - исправил файл anchor

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@
 import Slider from './slider.js';
 import Burger from './Burger.js';
 import ShirinkMenu from './ShirinkMenu.js';
-import Show_content from './Show_content.js'; 
-
+import Show_content from './show_content.js'; 
+import './anchor.js'; 
 
 
 new Burger(".menu__button-burger",".nav-menu");
