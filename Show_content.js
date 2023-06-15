6c627164f9d6097fb6bb21de228149f3986429da
Xpyst <qjq3@mail.ru>
2023-06-15 14:16:48 +0300

    перенёс js файлы в корень для работы Git Pages

---
+++ Show_content.js
@@ -0,0 +1,91 @@
+
+export default class Show_content{
+    constructor(){
+      this.about_section();
+      this.service_section();
+    }
+
+    about_section(){
+
+        let left_element = document.querySelector('.about-section__img');
+        let right_element = document.querySelector('.about-section__text-content');
+        let count = 2;
+
+            function onEntry(entry) {
+                entry.forEach(change => {
+                const { target, isIntersecting } = change;
+                  if (isIntersecting) {
+                    target.classList.add('display_opacity');
+                    if(count != 0){
+                        //для оптимизациия что бы каждый раз не проходить по DOM 
+                        var elementLeft = target.querySelector(".about-section__img");
+                        var elementRight = target.querySelector(".about-section__text-content");
+                        count--;
+
+                    }
+                    
+                    
+                    if(elementLeft != null && elementRight != null){
+                        elementLeft.classList.add('translateX');
+                        elementRight.classList.add('translateX');
+                    }
+                   
+                  }
+                });
+              }
+        
+            
+            let options = { threshold: [0.1] }; //на каком % срабатывает при попадании sectiond в область viewport
+            let observer = new IntersectionObserver(onEntry, options);
+            let elements = document.querySelectorAll('section');
+            for (let elm of elements) {
+                observer.observe(elm);
+            }
+
+    } 
+
+    service_section(){
+
+      function onEntry(entry) {
+        entry.forEach(change => {
+         const { target, isIntersecting } = change; //Деструктурирующее присваивание
+          if (isIntersecting) {
+    
+            target.classList.add('text-content__main-container-opacity');
+            target.classList.add("text-content__main-container-margin");
+          }
+        });
+      }
+    
+      this.h2_animation_content();
+      let options = { threshold: [0.3] }; //на каком % срабатывает при попадании sectiond в область viewport
+      let observer = new IntersectionObserver(onEntry, options);
+      let elements = document.querySelectorAll('.text-content__main-container');
+      console.log(elements);
+      for (let elm of elements) {
+          observer.observe(elm);
+      }
+
+    }
+  
+    h2_animation_content(){
+
+        function onEntry(entry) {
+          entry.forEach(change => {
+          const { target, isIntersecting } = change;
+            if (isIntersecting) {
+              target.classList.add('header-h2_active');
+            }
+          });
+        }
+
+
+        let options = { threshold: [0.1] }; //на каком % срабатывает при попадании sectiond в область viewport
+        let observer = new IntersectionObserver(onEntry, options);
+        let elements = document.querySelectorAll('.header-h2_letter_spacing');
+        console.log(elements);
+        for (let elm of elements) {
+            observer.observe(elm);
+        }
+    }
+}
---
+++ anchor.js
@@ -0,0 +1,62 @@
+
+(function(){
+  const changeNav = (entries, observer) => {
+    entries.forEach((entry) => {
+      // чекаем, то элемент пересекает наблюдаемую область более, чем на 65%
+      if(entry.isIntersecting && entry.intersectionRatio >= 0.65) {
+        // удаляем активный класс у элемента меню
+        let a = document.querySelector('.list__link-active');
+        a.classList.remove('list__link-active');
+        // получаем ID секции, которая текущая
+        let id = entry.target.getAttribute('id');
+        // обращаемся к ссылке меню, у которой href равен ID секции
+        let newLink = document.querySelector(`[href="#${id}"]`).classList.add('list__link-active');
+      }
+    });
+  }
+  
+  const options = {
+    threshold: 0.65
+  }
+  
+  const observer = new IntersectionObserver(changeNav, options);
+  
+  // передаём все секции в обсервер
+  const sections = document.querySelectorAll('section');
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+}());
+
+
+// <script>
+
+// <!-- const changeNav = (entries, observer) => {
+// entries.forEach((entry) => {
+//     const { target, isIntersecting } = entry; //Деструктурирующее присваивание
+//   // чекаем, то элемент пересекает наблюдаемую область более, чем на 55%
+//   if(isIntersecting && entry.intersectionRatio >= 0.55) {
+//     // удаляем активный класс у элемента меню
+//     document.querySelector('.list__link-active').classList.remove('list__link-active');
+//     // получаем ID секции, которая текущая
+//     let dateAtribut = target.dataset.anchor;
+//     // обращаемся к ссылке меню, у которой href равен ID секции
+//     let newLink = document.querySelector(`[href="#${dateAtribut}"]`).classList.add('list__link-active');
+//   }
+// });
+// }
+
+// const options = {
+// threshold: 0.55 //можно поиграться
+// }
+
+// const observer = new IntersectionObserver(changeNav, options);
+
+// // передаём все секции в обсервер 
+// const sections = document.querySelectorAll('section');
+// sections.forEach((section) => {
+// observer.observe(section);
+// });  -->
+
+// </script>
+
