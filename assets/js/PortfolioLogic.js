2a94d72e2ca3e09ecea4d4a452720a7ef2319a67
Xpyst <qjq3@mail.ru>
2023-06-19 03:58:52 +0300

    js PorfolioLogic + fslihtbox

---
+++ assets/js/PortfolioLogic.js
@@ -0,0 +1,98 @@
+
+
+export default class PortfolioLogic{
+
+    element_list = [...document.querySelectorAll('.portfolio_section__list')];
+    element_block = [...document.querySelectorAll('.portfolio_section__rewiev-block-info')];
+    
+    element_assoativ = new Map();
+
+   
+
+    constructor(){
+        this.set_element_assoativ();
+        this.menu_switching();
+        this.menu_select();
+    }
+
+    
+
+    set_element_assoativ(){
+        let g = 0 , t = 0;
+        while(g != 1){
+            let timeMass = [];
+            while(t < this.element_block.length){
+                timeMass.push(this.element_block[t]);
+                t++;
+            }
+            this.element_assoativ.set(this.element_list[g], timeMass)
+            g++;
+        }
+
+        for(let i = 1; i < this.element_list.length; i++ ){
+            let timeMass = [];
+            for(let j = 0; j < this.element_block.length; j++){
+                if(this.element_list[i].dataset.menulist == this.element_block[j].dataset.menulist){
+                    timeMass.push(this.element_block[j]);
+                }
+                this.element_assoativ.set(this.element_list[i], timeMass)
+
+                
+            }
+            
+        }
+        // for (let value of this.element_assoativ.values()) {
+        //     console.log("1:" , value);
+        // }
+
+        // for (let value of this.element_assoativ.keys()) {
+        //     console.log(value);
+        // }
+
+        // console.log(this.element_assoativ["all"]);
+        console.log(this.element_assoativ.get(this.element_list[0]));
+    }
+
+    menu_switching(){
+        for(let i = 0; i < this.element_list.length; i++){
+
+            this.element_list[i].addEventListener("click",(event)=>{
+
+                this.element_list.forEach(event => {
+                    if(event.classList.contains("portfolio_section__list-active")){
+                        event.classList.remove("portfolio_section__list-active");
+                    }
+                });
+                event.target.classList.add("portfolio_section__list-active");
+                
+                this.menu_select();
+            });
+
+        }
+     
+    }
+
+    menu_select(){
+
+        for(let i = 0; i < this.element_list.length ; i++){
+            if(this.element_list[i].classList.contains("portfolio_section__list-active")){
+                for(let j = 0; j < this.element_block.length; j++){
+                    if(this.element_block[j].classList.contains("portfolio_section__rewiev-scale")){
+                        this.element_block[j].classList.remove("portfolio_section__rewiev-scale");
+                        // this.element_block[j].classList.add("display_none");
+                    }
+                }
+                let massBlock = this.element_assoativ.get(this.element_list[i]);
+
+                for(let k = 0;  k < massBlock.length; k++){
+                    massBlock[k].classList.add("portfolio_section__rewiev-scale");
+                    // if(!massBlock[k].classList.contains("portfolio_section__rewiev-scale")){
+                        
+                    //     // massBlock[k].classList.add("portfolio_section__rewiev-scale");
+                    // }
+                }
+            }
+
+        }
+    }
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,12 @@ import Burger from './Burger.js';
 import ShirinkMenu from './ShirinkMenu.js';
 import Show_content from './Show_content.js'; 
 import './anchor.js'; 
+import './fslightbox.js'
+import PortfolioLogic from './PortfolioLogic.js'
 
 
 new Burger(".menu__button-burger",".nav-menu");
 new Slider(".content-section_clips");
 new ShirinkMenu();  
 new Show_content();
+new PortfolioLogic();
