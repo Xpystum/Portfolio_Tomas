0a85b3e1c7a197b8288df6c8359b315e0c51dd38
Xpyst <qjq3@mail.ru>
2023-07-09 23:03:16 +0300

    Preloader through Promise

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const mediaFiles = document.querySelectorAll("img");
+const mediaFiles = [...document.querySelectorAll("img")];
     const elPreloader = document.querySelector(".preloader");
     const elPreloaderInteger = document.querySelector(".preloader__integer");
     const elTags = document.querySelectorAll("header,main,footer");
@@ -8,41 +8,51 @@ const mediaFiles = document.querySelectorAll("img");
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
-    let i = 0;
-
+    let count = 0;
     console.log(mediaFiles);
-
     Array.from(mediaFiles).forEach((file,index) => {
-
-
-        file.onload = () =>{
-            i++;
-            elPreloaderInteger.innerHTML = ((i*100) / mediaFiles.length).toFixed();
-            console.log(i);
-
-            
-            if(i === mediaFiles.length){
-                elTags.forEach((el) =>{
-                    console.log(el.className);
-                    switch(el.className){
-                        case "header header_hide":
-                            el.classList.toggle("header_hide")
-                            break;
-                        case "main main_hide":
-                            el.classList.toggle("main_hide")
-                            break;
-                        case "footer footer_hide":
-                            el.classList.toggle("footer_hide")
-                            break;
-                    }
-                    elPreloader.classList.toggle("preloader_hide");
-                    elBody.classList.toggle("body-hiden");
-                    elPreloaderInteger.innerHTML = 100;
-                    
-                })
-            }
-
-        }
-        
+        let promise = img_count(file, count);
+        promise.then(
+            script => (console.log("картинки загружены")),
+            error => (console.log("ошибка загрузок картинок"))
+        )
+        count++;
     })
 })
+
+
+
+function img_count(file, count){
+
+    return new Promise((resolve, reject) => {
+
+    file.onerror = () => { count++;  reject(new Error(`Ошибка загрузки картинки ${file}`)) };
+
+    file.onload = () => { 
+        count++;
+        console.log(count);
+        elPreloaderInteger.innerHTML = ((count*100) / mediaFiles.length).toFixed();
+        resolve(console.log(file, " успешно загружен"));
+    }
+
+    if(count === mediaFiles.length){
+        elTags.forEach((el) => {
+            switch(el.className){
+                case "header header_hide":
+                    el.classList.remove("header_hide")
+                    break;
+                case "main main_hide":
+                    el.classList.remove("main_hide")
+                    break;
+                case "footer footer_hide":
+                    el.classList.remove("footer_hide")
+                    break;
+            }
+        });
+            elPreloader.classList.toggle("preloader_hide");
+            elBody.classList.toggle("body-hiden");
+            elPreloaderInteger.innerHTML = 100;
+    }
+
+    });
+}
